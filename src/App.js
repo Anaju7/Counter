cf07ffed0413f1d50595dd45b6e2088936f8f533
Ana julia <anajulia.anaju7@gmail.com>
2020-09-16 23:07:57 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import Title from './components/titulo'
+
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      < Title />
+      <h2 className="num">{count}</h2>
+      <div className="sessao-bnt">
+      <button className="btn" onClick={() => setCount(count + 1)} >+</button>
+      <button className="btn reset" onClick={() => setCount(0)}>reset</button>
+      <button className="btn" onClick={() => setCount(count <= 0 ? 0: count -1)}>-</button>
+      </div>
+      <p className="dev">feito por @AnaJu</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/titulo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from '../components/style.css';
+
+const Title = () =>(
+   <h1 className="titulo">Counter</h1>
+);
+
+export default Title;
